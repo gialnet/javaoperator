af7e311bc122fb50eadefe27480516a42e2a63b7
gialnet <antonio.gialnet@gmail.com>
2021-10-20 17:19:04 +0200

    first commit

---
+++ src/main/java/com/vivaldispring/javaoperator/JavaoperatorApplication.java
@@ -0,0 +1,13 @@
+package com.vivaldispring.javaoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaoperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaoperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivaldispring/javaoperator/JavaoperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivaldispring.javaoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaoperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
